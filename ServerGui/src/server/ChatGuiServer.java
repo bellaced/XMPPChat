ee47d5b2eb07cd90519195ba461ea88b33ce68b5
green327 <31414470+green327@users.noreply.github.com>
2017-11-28 16:14:14 -0800

    Fixed server gui popup error

--- ServerGui/src/server/ChatGuiServer.java
+++ ServerGui/src/server/ChatGuiServer.java
@@ -15,7 +15,7 @@ public class ChatGuiServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Gui().setVisible(true);
+        new Server.Gui().setVisible(true);
     }
     
 }
